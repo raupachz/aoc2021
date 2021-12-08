0e9020b9d544641f3ea6dd81f891d6b41b344b69
Björn Raupach <raupach@me.com>
2021-12-08 13:56:09 +0100

    Day 06

--- src/main/java/io/raupach/AdventOfCode06b.java
+++ src/main/java/io/raupach/AdventOfCode06b.java
@@ -1,14 +1,17 @@
 package io.raupach;
 
+import javax.swing.plaf.basic.BasicButtonUI;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static java.lang.System.arraycopy;
-import static java.lang.System.out;
+import static java.lang.System.*;
 
 public class AdventOfCode06b {
 
@@ -17,58 +20,37 @@ public class AdventOfCode06b {
         String line = input[0];
         String[] tokens = line.split(",");
 
-        long fish = 0L;
+        // We do no care about the position of the fishy. All we need to track is the
+        // total numbers of fish 8 years old, of fish 6 years old...
+        long[] fish = new long[9];
 
-        // Problem: Huge Memory needed and Java Arrays length is an int.
-        // Looks like it won't fit into memory, how about a file?
-        // We can count with a long instead of an int.
-        File tape = new File("src/main/resources", "memory.bin");
-        if (tape.exists()) {
-            tape.delete();
+        // Load inital data
+        for (int i = 0; i < tokens.length; i++) {
+            int index = Integer.parseInt(tokens[i]);
+            fish[index] = fish[index] + 1;
         }
 
-        try {
-            RandomAccessFile mem = new RandomAccessFile(tape, "rw");
-            // load initial fish into file
-            for (String token : tokens) {
-                int age = Integer.parseInt(token);
-                mem.writeByte(age);
-                fish++;
-            }
+        while (days-- > 0) {
+            long n = fish[0];
+            fish[0] = fish[1];
+            fish[1] = fish[2];
+            fish[2] = fish[3];
+            fish[3] = fish[4];
+            fish[4] = fish[5];
+            fish[5] = fish[6];
+            fish[6] = fish[7];
+            fish[7] = fish[8];
 
-            int n = days;
-            while (n-- > 0) {
-                System.out.println("Day " + (days - n));
-                // rewind memory
-                mem.seek(0);
-                // new fish to spawn
-                int spawns = 0;
-
-                for (long i = 0; i < fish; i++) {
-                    long pos = mem.getFilePointer();
-                    int age = mem.readByte();
-                    if (age == 0) {
-                        age = 6;
-                        spawns++;
-                    } else {
-                        age = age - 1;
-                    }
-                    mem.seek(pos);
-                    mem.writeByte(age);
-                }
-                if (spawns > 0) {
-                    for (int i = 0; i < spawns; i++) {
-                        mem.writeByte(8);
-                    }
-                    fish = fish + spawns;
-                }
-            }
-            mem.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            fish[6] = fish[6] + n;
+            fish[8] = n;
         }
 
-        return fish;
+        // Total number is the number of fish in each age group
+        long n = 0;
+        for (int i = 0; i < fish.length; i++) {
+            n += fish[i];
+        }
+        return n;
     }
 
     public long lanternfish(Path path, int days) {
